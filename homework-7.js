0fb0edef565ff09a50236ab951c7c8db1145f8d3
RailFarik <railfarhytdinov4@gmail.com>
2026-07-25 20:47:17 +0300

    feature/07-homework

---
+++ homework-7.js
@@ -0,0 +1,56 @@
+function tempCity(city, temperature) {
+    console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию.`)
+}
+
+tempCity('Нижнекамске', '30')
+
+                                                    
+
+const speedLight = 1079252848
+
+function checkSpeed(speed){
+
+if (speed > speedLight) {
+    console.log('Сверхсветовая скорость')
+} else if (speedLight === speed) {
+    console.log('Скорость света')
+} else {
+    console.log('Субсветовая скорость')
+}
+}
+
+checkSpeed(9999999999)
+checkSpeed(1003985334)
+checkSpeed(1079252848)
+
+                                                     
+
+const socks = 'Носки'
+const price = 33 
+
+function buySocks(budget) {
+
+if (budget >= price) {
+    console.log(`${socks} приобретены. Спасибо за покупку!`)
+} else {
+    const difference = price - budget;
+    console.log(`Вам не хватает ${difference}$, пополните баланс.`)
+}
+}
+
+buySocks(40);
+buySocks(24);
+
+                                                    
+
+function js(message) {
+    console.log(`В ${message} легко запутаться и он сложный.`)
+}
+
+js('JavaScript')
+
+                                                    
+
+const productName = 'car';
+const productPrice = 2500000;
+const productInStock = true;
