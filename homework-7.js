981f758535d73cc55ea79f6d15d95b006a588291
RailFarik <railfarhytdinov4@gmail.com>
2026-07-26 10:34:39 +0300

    feature/07-homework

--- homework-7.js
+++ homework-7.js
@@ -1,55 +1,45 @@
-function tempCity(city, temperature) {
-    console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию.`)
+function showTemperature(city, temperature) {
+    console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию.`);
 }
 
-tempCity('Нижнекамске', '30')
+showTemperature('Нижнекамске', '30');
 
-                                                    
+const speedLight = 1079252848;
 
-const speedLight = 1079252848
-
-function checkSpeed(speed){
-
-if (speed > speedLight) {
-    console.log('Сверхсветовая скорость')
-} else if (speedLight === speed) {
-    console.log('Скорость света')
-} else {
-    console.log('Субсветовая скорость')
-}
+function checkSpeed(speed) {
+    if (speed > speedLight) {
+        console.log('Сверхсветовая скорость');
+    } else if (speedLight === speed) {
+        console.log('Скорость света');
+    } else {
+        console.log('Субсветовая скорость');
+    }
 }
 
-checkSpeed(9999999999)
-checkSpeed(1003985334)
-checkSpeed(1079252848)
+checkSpeed(9999999999);
+checkSpeed(1003985334);
+checkSpeed(1079252848);
 
-                                                     
-
-const socks = 'Носки'
-const price = 33 
+const socks = 'Носки';
+const price = 33;
 
 function buySocks(budget) {
-
-if (budget >= price) {
-    console.log(`${socks} приобретены. Спасибо за покупку!`)
-} else {
-    const difference = price - budget;
-    console.log(`Вам не хватает ${difference}$, пополните баланс.`)
-}
+    if (budget >= price) {
+        console.log(`${socks} приобретены. Спасибо за покупку!`);
+    } else {
+        const difference = price - budget;
+        console.log(`Вам не хватает ${difference}$, пополните баланс.`);
+    }
 }
 
 buySocks(40);
 buySocks(24);
 
-                                                    
-
-function js(message) {
-    console.log(`В ${message} легко запутаться и он сложный.`)
+function showMessage(message) {
+    console.log(`В ${message} легко запутаться и он сложный.`);
 }
 
-js('JavaScript')
-
-                                                    
+showMessage('JavaScript');
 
 const productName = 'car';
 const productPrice = 2500000;
